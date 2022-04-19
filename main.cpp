db05bbd1dc65e9549d886aa549ba18e60e9de0be
Vasilis Milionis <v_milionis@yahoo.com>
2022-04-19 22:34:46 +0300

    Implements timeMe function and runs it in main for the requested n

--- main.cpp
+++ main.cpp
@@ -1,43 +1,28 @@
 #include "utils.h"
+#include "graph_generators.h"
 #include "my_algorithms.h"
 #include "testbench.h"
 
 
 int main(int argc, char *argv[]) {
-    // bool x, y;
-    // leda::node v;
-    leda::list<leda::node> V1;
-    leda::list<leda::node> V2;
     // Initialize a random graph G
     leda::graph G;
+    int nested_n[] = {12000, 48000, 100000};
+    int ring_n[] = {20001, 50001, 100001};
+    int levels_n[] = {4*600, 4*1200, 4*1800};
+    Durations d;
     // random_connected_graph(G);
     // test_graph(G);
     // nestedSquares_graph(G, 12000);
     // ring_graph(G, 21);
+    // fourLevel_graph(G, 16);
 
-    // fourLevel_graph(G, 4);
-    // printGraph(G, "csacademy");
-    // std::cout << "Print is done!" << std::endl;
-    // x = my_bipar_checker(G, myV1, myV2);
-
-    // // Make graph undirected
-    // addComplementaryEdges(G);
-    // y = leda::Is_Bipartite(G, V1, V2);
-
-    // // Output status
-    // if (x == y && areListsIdentical(V1, myV1) && areListsIdentical(V2, myV2)){
-    //     std::cout << "Leda and my implementation returned the same two lists" << std::endl;
-    // }
-    // else if (x != y) {
-    //     std::cout << "Leda returned with " << y << std::endl << "My implementation returned with " << x << std::endl;
-    // }
-    // else {
-    //     std::cout << "Lists returned do not match!" << std::endl;
-    //     printLedaList(V1, "V1: ");
-    //     printLedaList(V2, "V2: ");
-    //     printLedaList(myV1, "myV1: ");
-    //     printLedaList(myV2, "myV2: ");
-    // }
-
-    timeMe(&ring_graph, G, 10, &my_bipar_checker, V1, V2);
+    for(int i=0; i<3; i++) {        
+        d = timeMe(&nestedSquares_graph, G, nested_n[i]);
+        d.print("Nested Squares Graph");
+        d = timeMe(&ring_graph, G, ring_n[i]);
+        d.print("Ring Graph");
+        d = timeMe(&fourLevel_graph, G, levels_n[i]);
+        d.print("Four Levels Graph");
+    }
 }
--- testbench.h
+++ testbench.h
@@ -1,24 +1,64 @@
 #include "utils.h"
 #include "my_algorithms.h"
+#include <chrono>
+#include <stdexcept>
 
-void timeMe(leda::graph& G, int n, void (*graphFunc)(leda::graph&, int)) {
-    // Helper nodes, edges, lists etc
+struct Durations {
+    std::chrono::duration<double, std::milli> myImpl, ledaImpl;
+    int numOfNodes;
+
+    void createFileIfNotExists(std::string filename) {
+        // If file already exists, return
+
+        // Else create and setup the first line as (leda, myImpl) - add a newline at the end
+    }
+
+    void save_into_csv(std::string filename) {
+        // Open file
+
+        // Write the measurment - add a newline at the end
+    }
+
+    void print(std::string typeOfGraph) {
+        std::cout << "..." << typeOfGraph << " n = " << numOfNodes << "..." << std::endl;
+        std::cout << "LEDA time:\t" << ledaImpl.count() << " ms" << std::endl;
+        std::cout << "myImpl time:\t" << myImpl.count() << " ms" << std::endl;
+    }
+};
+
+Durations timeMe(void (*graphFunc)(leda::graph&, int), leda::graph& G, int n) {
+    leda::list<leda::node> V1, V2, myV1, myV2;
+    int x, y;
 
     // Create the graph using the input function
+    G.clear();
     graphFunc(G, n);
     
-    // Configure chrono
-
     // Start timer
+    auto t0 = std::chrono::high_resolution_clock::now();
     
     // Run my_bipar_checker()
+    x = my_bipar_checker(G, myV1, myV2);
 
     // Capture time
+    auto t1 = std::chrono::high_resolution_clock::now();
+    
+    // Convert graph so I may run leda
+    addComplementaryEdges(G);
 
+    auto t2 = std::chrono::high_resolution_clock::now();
     // Run LEDAs implementation
+    y = leda::Is_Bipartite(G, V1, V2);
 
     // End timer
+    auto t3 = std::chrono::high_resolution_clock::now();
 
-    // Return times as a struct (or a pair)
-    
+    if(x != y) throw std::runtime_error("Your implementation is wrong!");
+
+    // Return durations as a struct (or a pair)
+    Durations d;
+    d.myImpl = t1 - t0;
+    d.ledaImpl = t3 - t2;
+    d.numOfNodes = n;
+    return d;
 }
