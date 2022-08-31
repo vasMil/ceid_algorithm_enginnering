3181ad6010a2400a4c48d0bfbe35d9b7ed7444b6
VasMil <v_milionis@yahoo.com>
2022-08-31 12:53:58 +0300

    Adds some checks for user input and refocus to the terminal after graph display

--- lab3/src/utils.h
+++ lab3/src/utils.h
@@ -2,10 +2,20 @@
 #include "visualization_utils.h"
 #include "AIMN91_DataStructure.h"
 
+// Lazy me: Reference to the code below
+// https://stackoverflow.com/questions/4654636/how-to-determine-if-a-string-is-a-number-with-c
+bool is_number(const std::string& s)
+{
+    std::string::const_iterator it = s.begin();
+    while (it != s.end() && std::isdigit(*it)) ++it;
+    return !s.empty() && it == s.end();
+}
+
 int cli(AIMN91_DataStructure& AIMN91) {
     #if VISUALIZE
-        // save_graph_state(AIMN91);
-        renderGraphFile();
+        save_graph_state(AIMN91);
+        auto viewer_to_close = renderGraphFile();
+        std::cout << "Please do not close the PNG viewer, I will kill the window on exit!" << std::endl;
     #endif
     /* PROMPT USER TO ADD EDGES - REQUEST FOR MINIMAL PATH - LENGTH */
     auto const command_regex = std::regex(
@@ -30,32 +40,63 @@ int cli(AIMN91_DataStructure& AIMN91) {
         std::smatch regex_match;
         std::regex_match(command, regex_match, command_regex);
 
+        std::string arg[] = {
+            regex_match[2],
+            regex_match[3],
+            regex_match[4]
+        };
+
         if (regex_match[0] == "exit") {
+            #if VISUALIZE
+                // Should close the viewer window.
+                // TODO: Restructure VISUALIZATION proccess so you only have access to kill that window
+                std::string close_viewer_cmd = "xdotool windowkill " + viewer_to_close;
+                int i = system(close_viewer_cmd.c_str());
+                if (i != 0) {
+                    std::cout << "Failed to close the viewer - most likely has already been killed" << std::endl;
+                }
+            #endif
             return 0;
         }
 
-        // TODO: Add checks for the arguments
         if(regex_match[1] == "add") {
-            AIMN91.add(std::stoi(regex_match[2]), std::stoi(regex_match[3]), std::stoi(regex_match[4]));
+            for (auto i = 0; i < 3; i++) {
+                if(!is_number(arg[i])) {
+                    std::cout << "Invalid arguments" << std::endl;
+                    goto game_loop;
+                }
+            }
+            AIMN91.add(std::stoi(arg[0]), std::stoi(arg[1]), std::stoi(arg[2]));
             #if VISUALIZE
                 resetEdgeColors(AIMN91);
                 save_graph_state(AIMN91);
-                renderGraphFile();
+                viewer_to_close = renderGraphFile();
             #endif
         }
         else if (regex_match[1] == "decrease") {
+            for (auto i = 0; i < 3; i++) {
+                if(!is_number(arg[i])) {
+                    std::cout << "Invalid arguments" << std::endl;
+                    goto game_loop;
+                }
+            }
             AIMN91.decrease(std::stoi(regex_match[2]), std::stoi(regex_match[3]), std::stoi(regex_match[4]));
             #if VISUALIZE
                 resetEdgeColors(AIMN91);
                 save_graph_state(AIMN91);
-                renderGraphFile();
+                viewer_to_close = renderGraphFile();
             #endif
         }
         else if (regex_match[1] == "minimal_path") {
+            for (auto i = 0; i < 2; i++) {
+                if(!is_number(arg[i])) {
+                    std::cout << "Invalid arguments" << std::endl;
+                    goto game_loop;
+                }
+            }
             auto path = AIMN91.minimal_path(std::stoi(regex_match[2]), std::stoi(regex_match[3]));
             if (path.size() == 0) {
                 std::cout << "There is no path from " << regex_match[2] << " to " << regex_match[3] << "!" << std::endl;
-                continue;
             }
             std::cout << "minimal_path(" << regex_match[2] << "," << regex_match[3] << ") = ";
             for (auto it = path.begin(); it != std::prev(path.end()); it++) {
@@ -75,18 +116,27 @@ int cli(AIMN91_DataStructure& AIMN91) {
                     color[e] = "red";
                 }
                 save_graph_state(AIMN91);
-                renderGraphFile();
+                viewer_to_close = renderGraphFile();
             #endif
         }
         else if (regex_match[1] == "length") {
+            for (auto i = 0; i < 2; i++) {
+                if(!is_number(arg[i])) {
+                    std::cout << "Invalid arguments" << std::endl;
+                    goto game_loop;
+                }
+            }
             std::cout << "length(" << regex_match[2] << "," << regex_match[3] << ") = " << 
                 AIMN91.length(std::stoi(regex_match[2]), std::stoi(regex_match[3])) << std::endl;
         }
         else {
             std::cout << "Invalid operation - please try again!" << std::endl;
         }
+
+        game_loop:;
         std::cin.clear();
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        
     }
 }
 
--- lab3/src/visualization_utils.h
+++ lab3/src/visualization_utils.h
@@ -2,9 +2,60 @@
 #include "AIMN91_DataStructure.h"
 
 #if VISUALIZE
-    void renderGraphFile() {
-        int i = std::system("python3 scripts/renderGraphviz.py out/AIMN91_graph.dot");
+
+    // Opens a pipe and retrieves the output of cmd
+    // pipe_exec IS NOT MY CODE - REFERENCE: 
+    // https://stackoverflow.com/questions/478898/how-do-i-execute-a-command-and-get-the-output-of-the-command-within-c-using-po
+    std::string pipe_exec(const char* cmd) {
+        std::array<char, 128> buffer;
+        std::string result;
+        std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+        if (!pipe) {
+            throw std::runtime_error("popen() failed!");
+        }
+        while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+            result += buffer.data();
+        }
+        return result;
+    }
+    
+    // Produces a png and opens up a view using a python script
+    // Restates the terminal as the active window, after viewer launched
+    std::string renderGraphFile() {
+        int i = 0, j = 0;
+        // Get the id of the window - terminal
+        auto terminalWindowId = pipe_exec("xdotool getactivewindow");
+        std::string focus_terminal_cmd = "xdotool windowactivate " + terminalWindowId;
+
+        // Run the python script and expect it to open a png viewer
+        i = std::system("python3 scripts/renderGraphviz.py out/AIMN91_graph.dot");
         if(i != 0) std::cout << "Graph render failed!" << std::endl;
+
+        // Wait for the viewer to become the active window
+        auto curr_window_id = pipe_exec("xdotool getactivewindow");
+        while(curr_window_id == terminalWindowId) {
+            if(i != 0) break;
+            usleep(100);
+            curr_window_id = pipe_exec("xdotool getactivewindow");
+        }
+
+        // Restore the terminal as the active window
+        j = system(focus_terminal_cmd.c_str());
+
+        // Inform the user for any unexpected behaviour
+        if(j != 0) {
+            std::cout << "Terminal focus failed";
+            if(i != 0) {
+                std::cout << std::endl;
+            }
+            else {
+                std::cout << " - focus remains on the graph!" << std::endl;
+            }
+        }
+        if (curr_window_id != terminalWindowId) {
+            return curr_window_id;
+        }
+        return "";
     }
 
     // Clean color map of edges from previous minimal path request
@@ -38,4 +89,7 @@
         // Close the output stream
         dotFile.close();
     }
+
+
+
 #endif
